aaf0c92ee18e8c4e444beb9a38eafe804af73d8b
prshtshrdev-afk <prsht.shr.dev@gmail.com>
2026-08-18 13:22:42 +0530

    Add Cloudinary domain to next.config for migrated images

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "*.orientbell.com",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
