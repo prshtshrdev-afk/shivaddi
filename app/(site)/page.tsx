9c4a93c6ac472b31fee1fd9d77d1ca3e8d4c47fd
prshtshrdev-afk <prsht.shr.dev@gmail.com>
2026-08-18 14:09:37 +0530

    Remove Our Collection (CategoryGrid) from home page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,7 +5,6 @@ import HeroSlider from "@/components/home/hero-slider";
 import CategoryRail, {
   type CategoryTile,
 } from "@/components/home/category-rail";
-import CategoryGrid from "@/components/home/category-grid";
 import CollectionProductGrid from "@/components/home/collection-product-grid";
 import PromoBand from "@/components/home/promo-band";
 import FeaturedProducts from "@/components/home/featured-products";
@@ -298,7 +297,6 @@ export default async function HomePage() {
     <>
       <HeroSlider slides={slides} />
       <CategoryRail tiles={railTiles} />
-      <CategoryGrid categories={categories} />
       <CollectionProductGrid />
       <LargeSlabs products={slabProducts} />
       <StatStrip
